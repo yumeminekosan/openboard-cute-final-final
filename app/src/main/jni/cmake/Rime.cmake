9630afec33486850f7a0b87346265b666478bcba
Z User <z@container>
2026-02-13 13:12:45 +0000

    🐛 修复 utf8.h 缺失问题 - 添加 utf8cpp 库
    - 添加 utf8.h 头文件到 librime/include
    - 在 Rime.cmake 中添加 include 路径
    - 解决 librime-octagram 编译错误

--- app/src/main/jni/cmake/Rime.cmake
+++ app/src/main/jni/cmake/Rime.cmake
@@ -8,6 +8,9 @@ set(RIME_PLUGINS librime-lua librime-octagram librime-predict)
 # Add darts include path for librime (prism.h needs darts.h)
 include_directories(${CMAKE_SOURCE_DIR}/OpenCC/deps/darts-clone-0.32)
 
+# Add utf8.h include path for librime-octagram
+include_directories(${CMAKE_SOURCE_DIR}/librime/include)
+
 # symlink plugins
 foreach(plugin ${RIME_PLUGINS})
   if(NOT EXISTS "${CMAKE_SOURCE_DIR}/librime/plugins/${plugin}")
